1ca28d609013dfe4291ff769ceeb06efd05749ad
Atodqin <87917506+Atodqin@users.noreply.github.com>
2021-10-28 15:23:59 +0200

    Add files via upload

---
+++ Views/Taglia/FormModificaTaglia.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectWork.Views.Taglia
+{
+    public class FormModificaTagliaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
